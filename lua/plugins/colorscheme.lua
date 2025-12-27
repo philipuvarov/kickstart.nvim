e6058f705520cfa3bd40b937303380209a6ee736
philuvarov <spindelkryp@proton.me>
2025-12-27 19:23:59 +0100

    trasnapernt background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,12 @@ return {
   config = function(plugin)
     vim.opt.rtp:append(plugin.dir .. '/packages/neovim')
     vim.cmd [[colorscheme aura-dark]]
+
+    -- Make background transparent
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
   end,
 }
 
